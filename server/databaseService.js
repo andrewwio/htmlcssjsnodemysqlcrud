feaf02eca13416608b23aa24cc22dce8cf2bd664
Andrew Kain <hello@andreww.io>
2022-06-07 04:43:11 +0400

    removed createdatabase and createtable

--- server/databaseService.js
+++ server/databaseService.js
@@ -5,17 +5,26 @@ dotenv.config();
 
 // Create connection 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT
 });
 
 // Connect
 connection.connect((err) => {
-  if(err){
-    throw err;
-  }
-  console.log('MySQL connected!');
+if(err){
+  throw err;
+}
+console.log('MySQL connected!');
 });
+
+class DatabaseService {
+  static getDatabaseServiceInstance() {
+    return instance ? instance : new DatabaseService();
+  }
+}
+
+
+module.exports = DatabaseService;
--- server/server.js
+++ server/server.js
@@ -13,27 +13,6 @@ app.use(express.urlencoded({ extended : false }));
 
 const databaseService = require('./databaseService');
 
-// Create database
-app.get('/createdatabase', (req, res) => {
-  let sql = 'CREATE DATABASE htmlcssjsnodemysqlcrud';
-  db.query(sql, (err, result) => {
-    if(err) throw err;
-    console.log(result);
-    res.send('Database created!');
-  });
-});
-
-// Create table 
-app.get('/createpoststable', (req, res) => {
-  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, name VARCHAR(255), date DATETIME, PRIMARY KEY(id))';
-  db.query(sql, (err, result) => {
-    if(err) throw err;
-    console.log(result);
-    res.send('Posts table created!');
-  });
-});
-
-
 // Create
 app.post('/insert', (req, res) => {
 
