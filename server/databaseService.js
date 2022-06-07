6b4f2fdf4d887098443e9779fb93c8d1991e5669
Andrew Kain <hello@andreww.io>
2022-06-07 04:26:24 +0400

    24:42 basic set up

---
+++ server/databaseService.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+// Create connection 
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+// Connect
+connection.connect((err) => {
+  if(err){
+    throw err;
+  }
+  console.log('MySQL connected!');
+});
--- server/server.js
+++ server/server.js
@@ -2,12 +2,37 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mysql = require('mysql2');
+
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
+
+const databaseService = require('./databaseService');
+
+// Create database
+app.get('/createdatabase', (req, res) => {
+  let sql = 'CREATE DATABASE htmlcssjsnodemysqlcrud';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Database created!');
+  });
+});
+
+// Create table 
+app.get('/createpoststable', (req, res) => {
+  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, name VARCHAR(255), date DATETIME, PRIMARY KEY(id))';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('Posts table created!');
+  });
+});
+
 
 // Create
 app.post('/insert', (req, res) => {
